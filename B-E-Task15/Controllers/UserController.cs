f3e19265c89d8b64c087361fcd4cbe25565b6e49
suleymanibishov <suleyman22022@gmail.com>
2023-12-04 00:28:14 +0400

    Task

---
+++ B-E-Task15/Controllers/UserController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace B_E_Task15.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.Name = "Amir";
+            ViewBag.Surname = "Khan";
+            ViewBag.Age = 55;
+            ViewBag.Country = "Dehli";
+            ViewBag.PhoneNumber = "+233 023 222 03";
+            return View();
+        }
+    }
+}
